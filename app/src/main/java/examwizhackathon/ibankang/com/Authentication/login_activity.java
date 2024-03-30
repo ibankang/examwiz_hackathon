deb86e11040a6f9581a0c8c787d3cd7e63fda538
ibankang <mribankang@gmail.com>
2024-03-30 14:37:02 +0530

    added login_activity code

--- app/src/main/java/examwizhackathon/ibankang/com/Authentication/login_activity.java
+++ app/src/main/java/examwizhackathon/ibankang/com/Authentication/login_activity.java
@@ -1,16 +1,122 @@
 package examwizhackathon.ibankang.com.Authentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import examwizhackathon.ibankang.com.MainActivity;
 import examwizhackathon.ibankang.com.R;
 
 public class login_activity extends AppCompatActivity {
 
+    String guid = null;
+    public static final String Guid_KEY = "guid";
+    TextInputLayout emailEditText,passwordEditText;
+    Button loginBtn;
+    ProgressBar progressBar;
+    TextView signup_txt, forgot_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        emailEditText = findViewById(R.id.email_edit_txt);
+        passwordEditText = findViewById(R.id.password_edit_txt);
+        loginBtn = findViewById(R.id.login_btn);
+        progressBar = findViewById(R.id.progress_bar);
+
+        loginBtn.setOnClickListener((v) -> loginUser());
+
+        signup_txt = findViewById(R.id.signup_txt);
+        signup_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_activity.this, signup_activity.class));
+            }
+        });
+
+        forgot_txt = findViewById(R.id.forgot_txt);
+        forgot_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_activity.this, forgot_password_activity.class));
+            }
+        });
+    }
+    void loginUser(){
+        String email  = emailEditText.getEditText().getText().toString();
+        String password  = passwordEditText.getEditText().getText().toString();
+
+
+        boolean isValidated = validateData(email,password);
+        if(!isValidated){
+            return;
+        }
+
+        loginAccountInFirebase(email,password);
+
+    }
+
+    void loginAccountInFirebase(String email,String password){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        changeInProgress(true);
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                changeInProgress(false);
+                if(task.isSuccessful()){
+                    //login is success
+                    if(firebaseAuth.getCurrentUser().isEmailVerified()){
+                        //go to mainactivity
+                        startActivity(new Intent(login_activity.this, MainActivity.class));
+                        finish();
+                    }else{
+                        Toast.makeText(login_activity.this, "Email not verified, Please verify your email.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    //login failed
+                    Toast.makeText(login_activity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            loginBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean validateData(String email,String password){
+        //validate the data that are input by user.
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/examwizhackathon/ibankang/com/Authentication/splash_screen_activity.java
+++ app/src/main/java/examwizhackathon/ibankang/com/Authentication/splash_screen_activity.java
@@ -2,8 +2,14 @@ package examwizhackathon.ibankang.com.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import examwizhackathon.ibankang.com.MainActivity;
 import examwizhackathon.ibankang.com.R;
 
 public class splash_screen_activity extends AppCompatActivity {
@@ -12,5 +18,24 @@ public class splash_screen_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser==null){
+                    startActivity(new Intent(splash_screen_activity.this,login_activity.class));
+                }else{
+                    startActivity(new Intent(splash_screen_activity.this, MainActivity.class));
+                }
+                finish();
+            }
+        },3000);
+
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
